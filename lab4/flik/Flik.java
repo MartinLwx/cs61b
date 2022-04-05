1177072c6932bcfb4741fb897ebac880bd10b401
MartinLwx <MartinLwx@163.com>
2022-04-05 10:34:34 +0800

    FF

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -8,6 +8,10 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        // the origin code use `==` rather than equals
+        // `==` check if a and b are pointing at the same object
+        // Integer constant pool: java will cache [-128, 127]
+        // so `a == b` will fail when they are 128
+        return a.equals(b);
     }
 }
